f816694daeb83aeee44acb734fd279d1d5ff2aa1
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2023-10-25 21:42:37 +0100

    add an asynchronous command consumer

--- src/Command/Implementation/Eloquent/AsyncCommandConsumer.php
+++ src/Command/Implementation/Eloquent/AsyncCommandConsumer.php
@@ -4,18 +4,24 @@ declare(strict_types=1);
 
 namespace Guagua\Command\Implementation\Eloquent;
 
-use Guagua\ValueObject\CommandClass;
+use Criba\Criteria;
+use Guagua\Command\CommandBus;
 
 class AsyncCommandConsumer
 {
     public function __construct(
-
+        public CommandRepository $repository,
+        public CommandBus $bus
     ) {
         //
     }
 
-    public function consume(): void
+    public function consume(Criteria $criteria): void
     {
-        //
+        $commands = $this->repository->pull($criteria);
+
+        foreach($commands as $command) {
+            $this->bus->dispatch($command);
+        }
     }
 }
--- tests/Unit/Command/Implementation/Eloquent/AsyncCommandBusTest.php
+++ tests/Unit/Command/Implementation/Eloquent/AsyncCommandBusTest.php
@@ -1,16 +1,23 @@
 <?php
 
+use Guagua\Command\CommandBus;
+use Guagua\Command\CommandMapper;
+use Guagua\Command\Definition\CommandHandlerInterface;
+use Guagua\Command\Definition\CommandInterface;
 use Guagua\Command\Implementation\Eloquent\AsyncCommandBus;
+use Guagua\Command\Implementation\Eloquent\AsyncCommandConsumer;
 use Guagua\Command\Implementation\Eloquent\CommandModel;
 use Guagua\Command\Implementation\Eloquent\CommandRepository;
 use Guagua\Command\Implementation\Eloquent\UnprocessedCommandBatchCriteria;
+use Guagua\Container\CommandHandlerContainer;
+use Guagua\Instancer\Instancer;
 use Guagua\ValueObject\Uuid;
 use Tests\Fixture\DeletePostCommand;
 use Tests\Fixture\EmptyCommand;
 
 beforeEach(fn () => $this->bootEloquent());
 
-it('can store commands in the command repository', function (EmptyCommand $command) {
+it('can store commands in the command repository', function (CommandInterface $command) {
 
     $commandBus = new AsyncCommandBus(new CommandRepository);
     $commandBus->dispatch($command);
@@ -23,7 +30,7 @@ it('can store commands in the command repository', function (EmptyCommand $comma
     ],
 ]);
 
-it('can pull stored commands from the command repository', function (DeletePostCommand $command) {
+it('can pull stored commands from the command repository', function (CommandInterface $command) {
 
     $repository = new CommandRepository;
     $commandBus = new AsyncCommandBus($repository);
@@ -42,3 +49,31 @@ it('can pull stored commands from the command repository', function (DeletePostC
         new DeletePostCommand((string) Uuid::random()),
     ],
 ]);
+
+it('can consume events stored in the repository', function () {
+
+    $command = new EmptyCommand;
+    $handler = new class implements CommandHandlerInterface {
+        public static $callCounter = 0;
+
+        public function __invoke($argument): void
+        {
+            self::$callCounter++;
+        }
+    };
+
+    $repository = new CommandRepository;
+    $commandBus = new AsyncCommandBus($repository);
+    $commandBus->dispatch($command);
+
+    $mapper = new CommandMapper([$command::class => $handler::class]);
+    $container = (new Instancer())->get(CommandHandlerContainer::class);
+    $commandBus = new CommandBus($mapper, $container);
+
+    $commandConsumer = new AsyncCommandConsumer($repository, $commandBus);
+    $criteria = new UnprocessedCommandBatchCriteria;
+    $commandConsumer->consume($criteria);
+
+    expect($handler::$callCounter)->toBe(1);
+
+});
